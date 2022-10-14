f41e08fb3f10d8cd9635b57b7f784d9241214ca7
Hugo <112352124+Hugonogo@users.noreply.github.com>
2022-10-14 08:47:14 -0300

    Create comb.py

---
+++ comb.py
@@ -0,0 +1,24 @@
+#Combinação simples em python
+
+def fatorial(numero):
+    if(numero == 0):
+        return 1
+    else:
+        for i in range(1, numero):
+            numero = numero * i
+        
+        return numero
+
+def comb(n, k):
+    nmk = n - k
+    n = fatorial(n)
+    k = fatorial(k)
+    nmk = fatorial(nmk)
+    
+    return n / (k * nmk)
+
+
+n = int(input("--> "))
+k = int(input("--> "))
+
+print(f"C{n}{k} = {comb(n, k)}")
